125ecb5aade032b0b34a3877add642a1d9a64ce6
Karol Lisiewicz <karol.lisiewicz@gmail.com>
2018-11-10 19:49:24 +0100

    Time caching test.

--- lib/crypto/cache/cache_time_policy.dart
+++ lib/crypto/cache/cache_time_policy.dart
@@ -1,17 +1,21 @@
 import 'package:crypto_currency/crypto/cache/cache_policy.dart';
+import 'package:crypto_currency/crypto/time/date_time_provider.dart';
 
 class CacheTimePolicy implements CachePolicy {
+  final DateTimeProvider dateTimeProvider;
   DateTime _creationDate;
+
+  CacheTimePolicy(this.dateTimeProvider);
 
   @override
   bool isValid() {
     if (_creationDate == null) {
-      _creationDate = DateTime.now();
+      _creationDate = dateTimeProvider.get();
       return false;
     } else {
-      final now = DateTime.now();
-      final difference = _creationDate.difference(now);
-      if (difference.inMinutes > 5) {
+      final now = dateTimeProvider.get();
+      final difference = now.difference(_creationDate);
+      if (difference.inMinutes >= 5) {
         _creationDate = now;
         return false;
       } else {
--- lib/crypto/di/injector.g.dart
+++ lib/crypto/di/injector.g.dart
@@ -15,8 +15,10 @@ class _$Injector extends Injector {
             (c) => CoinMarketCapCurrencyService(c<Client>()));
     container.registerFactory<CryptoCurrencyRateDao,
         CryptoCurrencyRateInMemoryDao>((c) => CryptoCurrencyRateInMemoryDao());
+    container.registerFactory<DateTimeProvider, DateTimeNowProvider>(
+            (c) => DateTimeNowProvider());
     container.registerFactory<CachePolicy, CacheTimePolicy>(
-            (c) => CacheTimePolicy());
+            (c) => CacheTimePolicy(c<DateTimeProvider>()));
     container.registerSingleton<CryptoCurrencyRateRepository,
             CryptoCurrencyRestRepository>(
             (c) =>

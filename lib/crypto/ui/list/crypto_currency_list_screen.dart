9efd720b53c3d7a602afd7efcdce5fc7bfb66e31
Karol Lisiewicz <karol.lisiewicz@gmail.com>
2018-11-13 21:49:23 +0100

    Pull to refresh.

--- lib/crypto/ui/list/crypto_currency_list_screen.dart
+++ lib/crypto/ui/list/crypto_currency_list_screen.dart
@@ -6,15 +6,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kiwi/kiwi.dart' as kiwi;
 
-class CryptoCurrencyListScreen extends StatelessWidget {
+class CryptoCurrencyListScreen extends StatefulWidget {
   final CryptoCurrencyRateRepository _cryptoCurrencyRepository =
   new kiwi.Container()<CryptoCurrencyRateRepository>();
+
+  @override
+  _CryptoCurrencyListScreenState createState() =>
+      _CryptoCurrencyListScreenState();
+}
+
+class _CryptoCurrencyListScreenState extends State<CryptoCurrencyListScreen> {
+  List<CryptoCurrencyRate> cryptoCurrencyRates;
 
   @override
   Widget build(BuildContext context) =>
       Scaffold(
         appBar: _buildAppBar(context),
-        body: _buildBody(),
+        body: _buildBody(context),
       );
 
   Widget _buildAppBar(BuildContext context) =>
@@ -22,9 +30,16 @@ class CryptoCurrencyListScreen extends StatelessWidget {
         title: Text('Cryptocurrencies'),
       );
 
-  Widget _buildBody() {
+  Widget _buildBody(BuildContext context) {
+    if (cryptoCurrencyRates == null)
+      return _buildCryptoCurrencyListLoader();
+    else
+      return _buildCryptoCurrencyList(context);
+  }
+
+  Widget _buildCryptoCurrencyListLoader() {
     return FutureBuilder<Iterable<CryptoCurrencyRate>>(
-      future: _cryptoCurrencyRepository.findAll(),
+      future: widget._cryptoCurrencyRepository.findAll(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         return _buildFromSnapshot(context, snapshot);
       },
@@ -33,20 +48,35 @@ class CryptoCurrencyListScreen extends StatelessWidget {
 
   Widget _buildFromSnapshot(BuildContext context, AsyncSnapshot snapshot) {
     if (snapshot.connectionState == ConnectionState.done) {
-      if (snapshot.hasData)
-        return CryptoCurrencyList(
-          cryptoCurrencies: snapshot.data.toList(),
-          onValueSelected: (CryptoCurrencyRate cryptoCurrency) {
-            _navigateToCryptoCurrencyDetails(cryptoCurrency, context);
-          },
-        );
-      else
+      if (snapshot.hasData) {
+        cryptoCurrencyRates = snapshot.data.toList();
+        return _buildCryptoCurrencyList(context);
+      } else
         return SnackBar(
           content: Text(snapshot.error.toString()),
         );
     } else {
       return LinearProgressIndicator();
     }
+  }
+
+  Widget _buildCryptoCurrencyList(BuildContext context) =>
+      RefreshIndicator(
+        onRefresh: _refreshCurrencyRates,
+        child: CryptoCurrencyList(
+          cryptoCurrencies: cryptoCurrencyRates,
+          onValueSelected: (CryptoCurrencyRate cryptoCurrency) {
+            _navigateToCryptoCurrencyDetails(cryptoCurrency, context);
+          },
+        ),
+      );
+
+  Future<void> _refreshCurrencyRates() async {
+    final updatedCryptoCurrencyRates =
+    await widget._cryptoCurrencyRepository.findAll();
+    setState(() {
+      cryptoCurrencyRates = updatedCryptoCurrencyRates.toList();
+    });
   }
 
   void _navigateToCryptoCurrencyDetails(CryptoCurrencyRate value,
